b95985ac140509d5a2efb398b4d2f3a9e0a369dd
Shane0627 <sushifantasy@hotmail.com>
2020-08-11 19:18:28 +0800

    re

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,18 +1,16 @@
 var AC = document.getElementById("ac"); //清除鍵
 var del = document.getElementById("del"); //後退鍵
-var btn_txt = document.getElementsByClassName("btn_txt"); //按鍵輸入到input
-var dot = document.getElementsByClassName("dot"); //小數點
+var dot = document.getElementsByClassName("dot")[0]; //小數點
 var txt = document.getElementsByClassName("txt")[0]; //輸入畫面
 var displayinit = 0;
 
-
-
 AC.onclick = function () {
-    txt.value = '0';
+    txt.value = 0;
+    valueQ = 0;
 }
 
 del.onclick = function () {
-    console.log(txt.value.length);
+    // console.log(txt.value.length);
     if (txt.value.length == 1) {
         txt.value = "0";
     } else {
@@ -20,11 +18,20 @@ del.onclick = function () {
     }
 }
 
+dot.onclick = function(){
+    if(txt.value += dot.value){
+        // console.log(txt.value);
+        dot.disabled = true;
+    }else{
+        dot.enabled = true;
+    }
+}
+
 function numBtn(str) {
     if (displayinit == null) {
         txt.value = str;
         displayinit = 0;
-        return;
+        // return;
     };
 
     if (txt.value == 0) {
@@ -32,14 +39,13 @@ function numBtn(str) {
     } else {
         txt.value += str;
     }
+    console.log(str);
 }
 
 function operBtn(str) {
     equal();
-
     valueQ = txt.value;
     displayinit = str;
-
     txt.value = 0;
 }
 
@@ -62,35 +68,3 @@ function equal() {
             break;
     }
 }
-
-
-
-// for (var i = 0; i < btn_txt.length; i++) {
-//     btn_txt[i].onclick = function () {
-//         console.log(txt.value);
-//         if (txt.value == "" && this.value == ".") {
-//             txt.value = "0.";
-//         } else {
-//             if (!isNaN(this.value) || this.value == ".") {
-//                 if (txt.value.indexof(".") != -1) {
-//                     // if (txt.value != (".")) {
-
-//                     //     txt.value += this.value;
-//                     // }
-//                 } else {
-//                     txt.value += this.value;
-//                 }
-//             } else {
-//                 if (this.value != "=") {
-//                     evalStrAry[evalStrAry.length] = txt.value;
-//                     evalStrAry[evalStrAry.length] = this.value;
-//                     txt.value = "";
-//                 } else {
-//                     evalStrAry[evalStrAry.length] = txt.value;
-//                     txt.value = eval(evalStrAry.join(""));
-//                     evalStrAry = [];
-//                 }
-//             }
-//         }
-//     }
-// }
